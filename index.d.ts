61a2e048c6bec92ad8839c625002f97a6c83bc90
imposibrus <imposibrus@gmail.com>
2017-08-04 16:25:42 +0300

    added interfaces

--- index.d.ts
+++ index.d.ts
@@ -11,20 +11,20 @@ declare namespace LucidSDK {
     interface LucidSDK {
         setEnvironment(env: string): LucidSDK;
         setApiKey(apiKey: string): LucidSDK;
-        getGlobalDefinitions(bundles: string[] | string): SendPromise;
-        getSuppliers(): LucidSDK;
+        getGlobalDefinitions(bundles: string[] | string): SendPromise<GlobalDefinitions>;
+        getSuppliers(): SendPromise<SuppliersResponse>;
         getBusinessUnits(): SendPromise;
-        getStandardQuestions(CountryLanguageID: number): SendPromise;
+        getStandardQuestions(CountryLanguageID: number): SendPromise<StandardQuestionsResponse>;
         getCustomQuestions(CountryLanguageID: number): SendPromise;
         getQuestionText(CountryLanguageID: number, QuestionID: number): SendPromise;
-        getQuestionOptions(CountryLanguageID: number, QuestionID: number): SendPromise;
+        getQuestionOptions(CountryLanguageID: number, QuestionID: number): SendPromise<QuestionOptionsResponse>;
         getExchangeSurveys(SupplierCode: string | number): SendPromise;
         getAllocatedSurveys(SupplierCode: string | number): SendPromise;
         getAllocatedSurvey(SupplierCode: string | number, SurveyNumber: number): SendPromise;
         getRecentlyAllocatedSurveys(date: string | Date | number, SupplierCode: string | number): SendPromise;
         getSurveyGroups(SupplierCode: string | number, SurveyNumber: number): SendPromise;
-        getQualifications(SurveyNumber: number): SendPromise;
-        getQuotas(SupplierCode: string | number, SurveyNumber: number): SendPromise;
+        getQualifications(SurveyNumber: number): SendPromise<QualificationsResponse>;
+        getQuotas(SupplierCode: string | number, SurveyNumber: number): SendPromise<QuotasResponse>;
         createLink(SupplierCode: string | number, SurveyNumber: number, data: SupplierLinkCreate): SendPromise;
         updateLink(SupplierCode: string | number, SurveyNumber: number, data: SupplierLinkUpdate): SendPromise;
         getLink(SupplierCode: string | number, SurveyNumber: number): SendPromise;
@@ -65,5 +65,114 @@ declare namespace LucidSDK {
         tap(): SendPromise;
     }
 
-    interface SendPromise extends Promise<any> {}
+    interface SendPromise<T = any> extends Promise<T> {}
+
+    interface GenericDefinition {
+        Code: string;
+        Id: string;
+        IsActive?: boolean;
+        Name: string;
+        SortOrder: number;
+    }
+
+    interface GlobalDefinitions {
+        AllBidProbabilities: GenericDefinition[];
+        AllBidStatuses: GenericDefinition[];
+        AllCategoryLockOutDurations: GenericDefinition[];
+        AllCountries: GenericDefinition[];
+        AllCountryLanguages: GenericDefinition[];
+        AllIndustries: GenericDefinition[];
+        AllProposalTypes: GenericDefinition[];
+        AllQuestionClassifications: GenericDefinition[];
+        AllSampleTypes: GenericDefinition[];
+        AllStudyTypes: GenericDefinition[];
+        AllSupplierLinkTypes: GenericDefinition[];
+        AllSupplierPreferenceTypes: GenericDefinition[];
+        AllSupplierRequestStatuses: GenericDefinition[];
+        AllSupplierTrackingUrlTypes: GenericDefinition[];
+        AllSurveyPlatforms: GenericDefinition[];
+        AllSurveyStatuses: GenericDefinition[];
+        AllThirdPartyServices: GenericDefinition[];
+    }
+
+    interface QuestionOption {
+        OptionText: string;
+        ParentItemText: string | null;
+        Precode: string;
+        QuestionID: number;
+    }
+
+    interface Question {
+        IsCoreDemographic: boolean;
+        IsFeasibilityFactor: boolean;
+        LK_QuestionClassificationID: number | null;
+        Name: string;
+        QuestionID: number;
+        QuestionText: string;
+        QuestionType: string;
+        SurveyUse: number;
+        QuestionOptions?: QuestionOption[];
+    }
+
+    interface Supplier {
+        Name: string;
+        Code: string;
+    }
+
+    interface SupplierAccount {
+        AccountName: string;
+        Suppliers: Supplier[];
+    }
+
+    interface QuotaQuestion {
+        QuestionID: number;
+        LogicalOperator: string;
+        PreCodes: string[];
+    }
+
+    interface Quota {
+        SurveyQuotaID: number;
+        SurveyQuotaType: string;
+        QuotaCPI: number;
+        Conversion: number;
+        NumberOfRespondents: number;
+        Questions: QuotaQuestion[] | null;
+    }
+
+    interface Qualification {
+        SurveyNumber: number;
+        Questions: QuotaQuestion[];
+    }
+
+    interface ApiResponse {
+        ApiResult: number;
+        ApiResultCode: number;
+        ApiAccount: string;
+        AccountType: number;
+        ApiAccountStatus: number;
+        AccountCode: string;
+        ApiMessages: string[];
+        ResultCount: number;
+    }
+
+    interface StandardQuestionsResponse extends ApiResponse {
+        Questions: Question[];
+    }
+
+    interface QuestionOptionsResponse extends ApiResponse {
+        QuestionOption: QuestionOption[];
+    }
+
+    interface SuppliersResponse extends ApiResponse {
+        AccountsWithSuppliers: SupplierAccount[];
+    }
+
+    interface QuotasResponse extends ApiResponse {
+        SurveyNumber: number;
+        SurveyQuotas: Quota[];
+    }
+
+    interface QualificationsResponse extends ApiResponse {
+        SurveyQualification: Qualification;
+    }
 }
